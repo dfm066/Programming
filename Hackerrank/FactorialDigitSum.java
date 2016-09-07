501533e406c56794d89df80fd6b186cc7135fc8f
Itachi Uchiha <dfm066@icloud.com>
2016-09-08 02:20:09 +0530

    Project Euler +

--- Hackerrank/FactorialDigitSum.java
+++ Hackerrank/FactorialDigitSum.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 import java.math.BigInteger;
 
-public class Solution {
+public class FactorialDigitSum {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         BigInteger fact = new BigInteger("1");

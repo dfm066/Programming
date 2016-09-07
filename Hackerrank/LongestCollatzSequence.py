afdfed1d5211b636837fe12ffce7295cf202e75b
dfm066 <dfm066@icloud.com>
2016-09-07 16:47:14 +0530

    HackerRank project Euler

--- Hackerrank/LongestCollatzSequence.py
+++ Hackerrank/LongestCollatzSequence.py
@@ -1,34 +1,52 @@
+import time
+t= time.time()
 limit = 5000000
-collatz = {1:1}
+collatz = {1:1,0:0}
 tmp = []
-num = 0
-for i in range(2, limit+1):
-    if i not in collatz:
+num = 1
+cnt = 0
+while 2*num <= limit:
+    collatz[2*num] = collatz[num] + 1
+    num *= 2
+for i in range(3,limit+1):
+    if i & 1 and i not in collatz:
         tmp.clear()
         num = i
-        while num != 1:
-            if num not in collatz:
-                tmp.append(num)
-                if num & 1:
-                    num = num*3+1
-                else:
-                    num //= 2
-
+        while num != 1 and num not in collatz:
+            tmp.append(num)
+            if num & 1:
+                num = (num*3+1)//2
             else:
-                break
-    for j in range(0, len(tmp)-1):
+                num >>= 1
+        tmp.append(num)
+        bound = len(tmp)-1
+        # print(tmp)
+        # input()
+        for j in range(bound-1, -1, -1):
             # if tmp[j] <= limit:
-        collatz[tmp[j]] = len(tmp[j:]) + collatz[num]
+            if tmp[j] & 1:
+                collatz[tmp[j]] = collatz[tmp[j+1]] + 2
+            else:
+                collatz[tmp[j]] = collatz[tmp[j + 1]] + 1
+            k = tmp[j]
+            while 2*k <= limit:
+                collatz[2*k] = 1+collatz[k]
+                k *= 2
+                cnt += 1
+
 print(len(collatz))
-# print(collatz)
-print(max(collatz.keys()))
-# t = int(input())
-# for tc in range(t):
-#     n = int(input())
-#     m = 0
-#     num = 0
-#     for i in range(1, n+1):
-#         if m <= collatz[i]:
-#             m = collatz[i]
-#             num = i
-#     print(num)
+print(max(collatz.values()))
+print(time.time()-t)
+ans = []
+curr_max = 0
+curr = 0
+for i in range(0,limit+1):
+    if curr_max < collatz[i]:
+        curr_max = collatz[i]
+        curr = i
+    ans.append(curr)
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    print(ans[n])
+

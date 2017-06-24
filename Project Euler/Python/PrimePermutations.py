8a28707d0a33634eac82bf947f7a640af5525a35
Aniket Ghanawat <dfm066@icloud.com>
2017-06-24 11:43:10 +0530

    Updated utils.py dep

--- Project Euler/Python/PrimePermutations.py
+++ Project Euler/Python/PrimePermutations.py
@@ -1,15 +1,8 @@
-import itertools,time 
-
-def nprimes(n):
-    """ Returns  a list of primes < n """
-    sieve = [True] * n
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-    return [2] + [i for i in range(3,n,2) if sieve[i]]
+import itertools,time
+from utils import utils
 
 t1 = time.clock()
-primes = nprimes(10000)
+primes = utils.nprimes(10000)
 it = 0
 size = 4
 grp = 3

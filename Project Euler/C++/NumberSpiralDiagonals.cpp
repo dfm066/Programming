b02ffcf0b65512c75d32f8724429f8c482f46267
Aniket Ghanawat <dfm066@icloud.com>
2017-07-07 22:50:50 +0530

    Project Euler #28

--- Project Euler/C++/NumberSpiralDiagonals.cpp
+++ Project Euler/C++/NumberSpiralDiagonals.cpp
@@ -9,20 +9,18 @@ using namespace std;
 
 
 int main() {
-    long long sum,N,d,a;
+    unsigned long long sum,N,t1,t2,t3,n;
     int T;
     cin >> T;
     while(T--){
         cin >> N;
-        sum = 1;
-        d = 2;
-        a = 3;
-        for(;N > 1; N-=2){
-            sum = (sum + ( 2*(2*a + 3*d ) )% MOD) % MOD;
-            a += 3*d;
-            d += 2;
-            a +=d;            
-        }
+        n = (N - 1) / 2;
+        sum = t1 = t2 = t3 = 0;
+        t1 = 8 * (n % MOD) * (n % MOD + 1) * (2 * (n % MOD) + 1) / 3;
+        t2 = 2 * (n % MOD) * (n % MOD + 1);
+        t3 = 4 * (n % MOD);
+        sum = (t1 + t2 + t3 ) % MOD + 1;
+        // cout << n << " " << t1 << " " << t2 << " " << t3 << endl;
         cout << sum << endl;
     }
     return 0;

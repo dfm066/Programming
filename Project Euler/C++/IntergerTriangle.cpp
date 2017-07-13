6461430b0e4c7ffaf698540b8a9a09579cd35b39
Aniket Ghanawat <dfm066@icloud.com>
2017-07-13 23:01:16 +0530

    Project Euler + #39

---
+++ Project Euler/C++/IntergerTriangle.cpp
@@ -0,0 +1,130 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+const int MAX = 5000000;
+const int MAXL = MAX+1;
+const int LIMIT = MAX / 2;
+const int PRIM = MAX * 0.8;
+const int UAD[3][3][3] = {{
+                        {1, 2, 2},
+                        {-2, -1, -2},
+                        {2, 2, 3}
+                        },
+                        {{1, 2, 2},
+                        {2, 1, 2},
+                        {2, 2, 3}
+                        },
+                        {{-1, -2, -2},
+                        {2, 1, 2},
+                        {2, 2, 3}
+                        }};                  
+int p[MAXL];
+int maxp[MAXL];
+
+struct r_trig{
+    int a;
+    int b;
+    int c;
+    r_trig() {}
+    r_trig(int a,int b,int c) : a(a), b(b), c(c) {}
+    r_trig(const r_trig& x) : a(x.a), b(x.b), c(x.c) {}
+    r_trig(const r_trig&& x) noexcept : a(move(x.a)), b(move(x.b)), c(move(x.c)) {}
+    r_trig& operator=(const r_trig& x){
+        a = x.a;
+        b = x.b;
+        c = x.c;
+        return *this;
+    }
+    r_trig& operator=(r_trig&& x){
+        return *this;
+    }
+
+    friend ostream &operator<<( ostream &output, 
+         const r_trig& x ) { 
+         output << "a : " << x.a << " b : " << x.b << " c : " << x.c;
+         return output;            
+      }
+};
+r_trig f(r_trig a) { return a; }
+
+int triples[PRIM+1][3];
+
+int main() 
+{
+    int     curr_a,curr_b,curr_c;
+    int     flg = 0;
+    int     limit = MAX / 2;
+    int     cnt = 1;
+    queue<r_trig> q;
+    r_trig curr_triple;
+    triples[0][0] = 3;
+    triples[0][1] = 4;
+    triples[0][2] = 5;
+    q.emplace(3,4,5);
+    ++cnt;
+    while (!q.empty())
+    {
+        int tmp[3];
+        int size = q.size();
+        for(int i = 0; i < size; ++i){
+            curr_triple = q.front();
+            q.pop();
+            for(int j = 0; j < 3; ++j){
+                tmp[0] = tmp[1] = tmp[2] = 0;
+                for(int k = 0; k < 3; ++k){
+                    tmp[k] += curr_triple.a * UAD[j][0][k];
+                    tmp[k] += curr_triple.b * UAD[j][1][k];
+                    tmp[k] += curr_triple.c * UAD[j][2][k]; 
+                }
+                if(tmp[2] < LIMIT){
+                    q.emplace(tmp[0],tmp[1],tmp[2]);
+                    triples[cnt][0] = tmp[0];
+                    triples[cnt][1] = tmp[1];
+                    triples[cnt][2] = tmp[2];
+                    cnt += 1;
+                } else {
+                    ++flg;
+                }   
+            }
+            // cout << curr_triple << endl;
+        }   
+    }
+    // cout << "finished : " << cnt << " Flag : " << flg << endl;
+    for(int i = 0; i < cnt; ++i){
+        int sum = triples[i][0] + triples[i][1] + triples[i][2];
+        int mult = 1;
+        int perimeter = sum * mult;
+        while(perimeter > 0 && perimeter < MAXL){
+            ++p[perimeter];
+            mult += 1;
+            perimeter = sum * mult;
+           // cout << perimeter << " ";
+        }
+    }
+    // cout << "done0" << endl;
+    int curr_maxp,curr_p,ignore;
+    curr_maxp = curr_p = 0;
+    for(int i = 12; i < MAXL; i += 2){
+        maxp[i-1] = curr_maxp;
+        if(curr_p < p[i]){
+            curr_p = p[i];
+            curr_maxp = i;
+        }
+        maxp[i] = curr_maxp;
+    }
+    // cout << "done1" << endl;
+    int T,N;
+    ignore = scanf("%d",&T);
+    while(T--){
+        ignore = scanf("%d",&N);
+        ignore = printf("%d",maxp[N]);
+
+    }
+    
+    return 0;
+}
--- Project Euler/Python/tester.py
+++ Project Euler/Python/tester.py
@@ -1,2 +1,56 @@
-num = "1234"
-print(num[3:])
+import numpy as np
+import time
+# def gen_prim_pyth_trips(limit=None):
+#     u = np.mat(' 1  2  2; -2 -1 -2; 2 2 3')
+#     a = np.mat(' 1  2  2;  2  1  2; 2 2 3')
+#     d = np.mat('-1 -2 -2;  2  1  2; 2 2 3')
+#     uad = np.array([u, a, d])
+#     m = np.array([3, 4, 5])
+#     while m.size:
+#         m = m.reshape(-1, 3)
+#         if limit:
+#             m = m[m[:, 2] <= limit]
+#         yield from m
+#         m = np.dot(m, uad)
+# def gen_all_pyth_trips(limit):
+#     for prim in gen_prim_pyth_trips(limit):
+#         i = prim
+#         for _ in range(limit//prim[2]):
+#             yield i
+#             i = i + prim
+
+# N = int(input())
+# t = time.clock()
+# triples = list(gen_prim_pyth_trips(N // 2))
+# print(len(triples))
+# print(time.clock() - t)
+# p = [0] * (N+1)
+# for i in triples:
+#     sum = i[0] + i [1] + i[2]
+#     mult = 1
+#     while sum * mult <= N:
+#         p[sum * mult] += 1
+#         mult += 1
+# maxp = [0] * (N+1)
+# counter = 0
+# curr_max = 0
+# curr_p = 0
+# for i in p:
+#     if curr_max < i:
+#         curr_max = i
+#         curr_p = counter
+#     maxp[counter] = curr_p
+#     counter += 1
+# print(time.clock() - t)
+# T = int(input())
+# while T != 0:
+#     T -= 1
+#     N = int(input())
+#     print(maxp[N])
+
+m = np.array([3, 4, 5])
+while m.size:
+        m = m.reshape(-1, 3)
+        print(m)
+        if 100:
+            m = m[m[:, 2] <= 100]

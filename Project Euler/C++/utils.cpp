dbc00223421e5d9c5173e23a83290449bee411d7
dfm066 <dfm066@icloud.com>
2018-11-02 16:17:43 +0530

    Updated utils

--- Project Euler/C++/utils.cpp
+++ Project Euler/C++/utils.cpp
@@ -1,28 +1,5 @@
 #include "utils.h"
 
-std::vector<int> primes2n(int n) {
-  std::vector<bool> num_map(n + 1);
-  std::vector<int> primes;
-
-  std::fill(std::begin(num_map), std::end(num_map), true);
-  primes.reserve(sqrt(n));
-
-  for (int i = 2; i <= sqrt(n); ++i) {
-    if (num_map[i]) {
-      for (int sq = i * i, j = sq, k = 1; j <= n; ++k) {
-        num_map[j] = false;
-        j = sq + k * i;
-      }
-    }
-  }
-
-  for (int i = 2; i <= n; ++i) {
-    if (num_map[i])
-      primes.push_back(i);
-  }
-
-  return primes;
-}
 
 bool operator==(const bigint &x, const int i) {
   if (i == 0)

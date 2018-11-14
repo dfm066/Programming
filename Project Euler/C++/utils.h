83312d7a766decf60124e7dc9fa0b345b707e626
dfm066 <dfm066@icloud.com>
2018-11-15 01:14:28 +0530

    Project Euler #59

--- Project Euler/C++/utils.h
+++ Project Euler/C++/utils.h
@@ -4,6 +4,7 @@
 #include<assert.h>
 
 #include<iostream>
+#include<fstream>
 #include<vector>
 #include<algorithm>
 #include<chrono>
@@ -145,3 +146,14 @@ struct bigint {
 
   friend std::ostream &operator<<(std::ostream &out, const bigint &x);
 };
+
+void tokenize(const std::string &str, std::vector<std::string> &tokens,
+	      const std::string &delimiters = " ") {
+  std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+  std::string::size_type pos = str.find_first_of(delimiters, lastPos);
+  while (std::string::npos != pos || std::string::npos != lastPos) {
+    tokens.push_back(str.substr(lastPos, pos - lastPos));
+    lastPos = str.find_first_not_of(delimiters, pos);
+    pos = str.find_first_of(delimiters, lastPos);
+  }
+}

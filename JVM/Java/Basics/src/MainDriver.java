8157e4b38dea42885a45ea46e7696fa4322a7c76
dfm066 <dfm066@icloud.com>
2020-05-29 11:50:23 +0530

    Cleanup java code

---
+++ JVM/Java/Basics/src/MainDriver.java
@@ -0,0 +1,15 @@
+/**
+ * @author Itachi
+ *
+ */
+public class MainDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		OperatorBasics.run();
+		UnicodeDemo.run();
+	}
+
+}
---
+++ JVM/Java/Basics/src/Menu.java
@@ -0,0 +1,4 @@
+
+public interface Menu {
+	public static void run(){}
+}

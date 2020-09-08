707bec8cbb38272710548933d2165c57e8b750b6
dfm066 <itachisan066@outlook.com>
2020-09-08 19:04:33 +0530

    :recycle: Removed instrumentation noise.

--- C++/Algorithm/unique.cpp
+++ C++/Algorithm/unique.cpp
@@ -12,45 +12,56 @@
 int main() {
   const int SIZE = 100000;
   const int TEST = 100;
+  size_t cols = instrumented<double>::number_ops;
   std::vector<instrumented<int>> nums(SIZE);
-  std::generate(nums.begin(), nums.end(), [n = 0]() mutable {
-    n++;
-    return n % (SIZE/TEST);
-  });
   time_diff exec_t1, exec_t2;
-  std::vector<double> counters1(instrumented<double>::number_ops, 0.0), counters2(instrumented<double>::number_ops, 0.0);
+  std::vector<double> counters1(cols, 0.0), counters2(cols, 0.0);
+
   for (int i = 0; i < TEST; ++i) {
+    std::generate(nums.begin(), nums.end(), [n = 0]() mutable {
+      n++;
+      return n % (SIZE / TEST);
+    });
     std::sort(nums.begin(), nums.end());
     instrumented<int>::initialize(SIZE);
-    //   fmt::print("{}\n", nums);
+
     auto st = GET_HRTIME();
     my_unique(nums.begin(), nums.end());
     auto en = GET_HRTIME();
     exec_t1 += en - st;
+
     double* count_p = instrumented<int>::counts;
     for (int j = 0; j < counters1.size(); ++j) {
       counters1[j] += count_p[j];
     }
   }
+
   for (int i = 0; i < TEST; ++i) {
+    std::generate(nums.begin(), nums.end(), [n = 0]() mutable {
+      n++;
+      return n % (SIZE / TEST);
+    });
     std::sort(nums.begin(), nums.end());
     instrumented<int>::initialize(SIZE);
-    //   fmt::print("{}\n", nums);
+
     auto st = GET_HRTIME();
     my_unique_slow(nums.begin(), nums.end());
     auto en = GET_HRTIME();
     exec_t2 += en - st;
+
     double* count_p = instrumented<int>::counts;
     for (int j = 0; j < counters2.size(); ++j) {
       counters2[j] += count_p[j];
     }
   }
 
+  for (int j = 0; j < counters2.size(); ++j) {
+    counters1[j] /= TEST;
+    counters2[j] /= TEST;
+  }
   exec_t1 /= TEST;
   exec_t2 /= TEST;
 
-  //   fmt::print("{}\n", nums);
-  size_t cols = instrumented<double>::number_ops;
   size_t decimals[cols];
   *decimals = 0;
   std::fill(decimals + 1, decimals + cols, 0);

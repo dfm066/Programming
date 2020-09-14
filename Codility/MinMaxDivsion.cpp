59a04a001862881e8ba6b9ed7a9e3f52021c81b9
dfm066 <itachisan066@outlook.com>
2020-09-15 01:22:44 +0530

    :zap: Codility #L14-01

--- Codility/MinMaxDivsion.cpp
+++ Codility/MinMaxDivsion.cpp
@@ -1,103 +1,40 @@
-#include <cassert>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 #include <numeric>
+#include <algorithm>
 
-using namespace std;
-
-class BIT
-{
-    public:
-    BIT(const vector<int> &arr) {
-        m_arr.resize(arr.size() + 1, 0);
-        int idx = 0;
-        for(const auto &val : arr) {
-            update(idx, val);
-            ++idx;
-        }
+int isValid(const vector<int> &A, int K, const int &target) {
+    int pos = 0;
+    int prev = 0;
+    while(K > 0) {
+        if (pos == int(A.size())) return 1;
+        int curr = distance(begin(A), lower_bound(begin(A)+pos, end(A), prev+target+1));
+        if (curr > 0 && A[curr-1]-prev <= target) K--;
+        else if (curr == 0) return -1;
+        prev = A[curr-1];
+        pos = curr;
     }
-    
-    void update(int idx, int val) {
-        ++idx;
-        while (idx < (int)m_arr.size()) {
-            m_arr[idx] += val;
-            idx += idx & (-idx);
-        }
-    }
-    
-    int edge_sum(int end) const {
-        int sum = 0;
-        ++end;
-        assert(end < (int)m_arr.size());
-        while(end > 0) {
-            sum += m_arr[end];
-            end -= end & (-end);
-        }
-        return sum;
-    }
-    
-    int interval_sum(int start, int end) const {
-        return edge_sum(end) - edge_sum(start-1);
-    }
-    friend ostream& operator<<(ostream& out, const BIT &tree);
-    private:
-    vector<int> m_arr;
-};
-
-struct vblock {
-    int st;
-    int en;
-    int val;
-    friend ostream& operator<<(ostream& out, const vblock &b);
-};
-
-ostream& operator<<(ostream& out, const BIT &tree) {
-    out << "[";
-    for (const auto& v : tree.m_arr){
-        out << v << ", ";
-    }
-    out << "]";
-    return out;
-}
-
-ostream& operator<<(ostream& out, const vblock &b) {
-    out << "[" << b.st << ", " << b.en << "] => " << b.val;
-    return out;
-}
-
-void update_block(const BIT &tree, vblock &b) {
-    b.val = tree.interval_sum(b.st, b.en);    
+    if (pos != int(A.size())) return -1;
+    return 0;
 }
 
 int solution(int K, int M, vector<int> &A) {
-    if (K >= (int)A.size())
-     return *min_element(A.begin(), A.end());
-    if (K == 1)
-        return accumulate(A.begin(), A.end(), 0);
-    BIT tree(A);
-    // cout << "Debug : " << tree << endl;
-    vector<vblock> blocks;
-    int block_size = (int)A.size() / K;
-    int correct = (int)A.size() % K;
-    for (int i = 0; i < K - correct; i++) {
-        blocks.push_back({i*block_size, (i+1)*block_size - 1,
-            tree.interval_sum(i*block_size, (i+1)*block_size - 1)
-        });
+    int N = A.size();
+    int maxel = *max_element(begin(A), end(A));
+    if (K >= N) return maxel;
+    int tot = accumulate(begin(A), end(A), 0);
+    if (K == 1) return tot;
+    for (int i = 1; i < N; ++i) A[i] += A[i-1];
+    A.erase(unique(begin(A), end(A)), end(A));
+    int low = 1, high = tot, mid = 0;
+    vector<int> valid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        int res = isValid(A, K, mid);
+        if (res  >= 0) {
+            valid.emplace_back(mid);
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
-    
-    for (int i = K - correct, st = (K - correct)*block_size; i < K; i++, st += block_size + 1) {
-        blocks.push_back({st, st + block_size,
-            tree.interval_sum(st, st + block_size)
-        });
-    }
-    cout << "Debug : { ";
-    for(const auto &block : blocks) {
-        cout << block <<", ";
-    }
-    cout << "}" << endl;
-    
-    return min_element(blocks.begin(), blocks.end(), [&](const vblock &a, vblock &b) {
-                return a.val > b.val;       
-            })->val;
+    return *min_element(begin(valid), end(valid));
 }
